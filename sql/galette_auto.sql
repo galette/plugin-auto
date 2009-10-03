66fce6be65cdcd46e06a52ebba1edf4275d51733
Johan Cwiklinski <johan@x-tnd.be>
2009-10-03 08:01:11 +0000

    Tables were not in the correct order, foreign keys creation failed

--- sql/galette_auto.sql
+++ sql/galette_auto.sql
@@ -19,6 +19,62 @@ CREATE TABLE `galette_auto_brands` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
+-- Table structure for table `galette_auto_colors`
+--
+
+CREATE TABLE `galette_auto_colors` (
+  `id_color` int(11) NOT NULL AUTO_INCREMENT,
+  `color` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_color`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Table structure for table `galette_auto_finitions`
+--
+
+CREATE TABLE `galette_auto_finitions` (
+  `id_finition` int(11) NOT NULL AUTO_INCREMENT,
+  `finition` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_finition`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Table structure for table `galette_auto_models`
+--
+
+CREATE TABLE `galette_auto_models` (
+  `id_model` int(11) NOT NULL AUTO_INCREMENT,
+  `model` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `id_brand` int(11) NOT NULL,
+  PRIMARY KEY (`id_model`),
+  KEY `galette_models_brand` (`id_brand`),
+  CONSTRAINT `galette_models_brand` FOREIGN KEY (`id_brand`) REFERENCES `galette_auto_brands` (`id_brand`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Table structure for table `galette_auto_states`
+--
+
+CREATE TABLE `galette_auto_states` (
+  `id_state` int(11) NOT NULL,
+  `state` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_state`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Table structure for table `galette_auto_transmissions`
+--
+
+DROP TABLE IF EXISTS `galette_auto_transmissions`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `galette_auto_transmissions` (
+  `id_transmission` int(11) NOT NULL AUTO_INCREMENT,
+  `transmission` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_transmission`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
 -- Table structure for table `galette_auto_cars`
 --
 
@@ -58,26 +114,6 @@ CREATE TABLE `galette_auto_cars` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Table structure for table `galette_auto_colors`
---
-
-CREATE TABLE `galette_auto_colors` (
-  `id_color` int(11) NOT NULL AUTO_INCREMENT,
-  `color` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_color`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Table structure for table `galette_auto_finitions`
---
-
-CREATE TABLE `galette_auto_finitions` (
-  `id_finition` int(11) NOT NULL AUTO_INCREMENT,
-  `finition` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_finition`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
 -- Table structure for table `galette_auto_history`
 --
 
@@ -85,7 +121,7 @@ CREATE TABLE `galette_auto_history` (
   `id_car` int(11) NOT NULL,
   `id_adh` int(10) NOT NULL,
   `history_date` date NOT NULL,
-  `history_registration` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `car_registration` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `id_color` int(11) NOT NULL,
   `id_state` int(11) NOT NULL,
   PRIMARY KEY (`id_car`,`id_adh`,`history_date`),
@@ -97,40 +133,4 @@ CREATE TABLE `galette_auto_history` (
   CONSTRAINT `galette_auto_history_member` FOREIGN KEY (`id_adh`) REFERENCES `galette_adherents` (`id_adh`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `galette_auto_history_color` FOREIGN KEY (`id_color`) REFERENCES `galette_auto_colors` (`id_color`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `galette_auto_history_state` FOREIGN KEY (`id_state`) REFERENCES `galette_auto_states` (`id_state`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Table structure for table `galette_auto_models`
---
-
-CREATE TABLE `galette_auto_models` (
-  `id_model` int(11) NOT NULL AUTO_INCREMENT,
-  `model` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `id_brand` int(11) NOT NULL,
-  PRIMARY KEY (`id_model`),
-  KEY `galette_models_brand` (`id_brand`),
-  CONSTRAINT `galette_models_brand` FOREIGN KEY (`id_brand`) REFERENCES `galette_auto_brands` (`id_brand`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Table structure for table `galette_auto_states`
---
-
-CREATE TABLE `galette_auto_states` (
-  `id_state` int(11) NOT NULL,
-  `state` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_state`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Table structure for table `galette_auto_transmissions`
---
-
-DROP TABLE IF EXISTS `galette_auto_transmissions`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-CREATE TABLE `galette_auto_transmissions` (
-  `id_transmission` int(11) NOT NULL AUTO_INCREMENT,
-  `transmission` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_transmission`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
